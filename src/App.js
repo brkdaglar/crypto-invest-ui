f8e81b9f0e7234d1a0cc23fac769903368d2ee91
brkdglr <dagburak3@gmail.com>
2022-08-17 11:33:34 +0300

    contract connection functions added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ChildList from "./pages/ChildList.js";
-import { onClickWallet, getContractAddress } from "./shared/contractDeploy.js";
-
-const onClickButton = () => {
-  getContractAddress();
-};
+import {
+  connectWallet,
+  addParent,
+  getParent,
+} from "./shared/contractDeploy.js";
 
 function App() {
-  return <button onClick={onClickWallet}>ConnectWallet</button>;
+  return (
+    <div>
+      <button onClick={connectWallet}>ConnectWallet</button>
+      <button onClick={addParent}> addParent </button>
+      <button onClick={getParent}> getParent </button>
+    </div>
+  );
 }
 
 export default App;
--- src/shared/contractDeploy.js
+++ src/shared/contractDeploy.js
@@ -4,21 +4,35 @@ import abi from "../data/contract.json";
 let contract;
 const CONTRACT_ADDRESS = "0x1B48129Fa3AA02d182f5e65811Cdc74D8ce554Bb";
 
-export const onClickWallet = async () => {
+export const connectWallet = async () => {
+  console.log("a");
   await window.ethereum.request({ method: "eth_requestAccounts" });
   const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-  const contract = new ethers.Contract(
-    CONTRACT_ADDRESS,
-    abi,
-    provider.getSigner()
-  );
+  contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider.getSigner());
 
-  console.log(contract.address);
+  console.log(provider.getSigner());
 
   //const tx = await contract.setBalanceAccessAge(25);
   //const txResult = await tx.wait();
   //console.log("Balance: ", { tx, txResult });
+  console.log(contract);
+};
+
+export const addParent = async () => {
+  console.log("addParent");
+  console.log(contract);
+  const tx = await contract.addParent("Burak", "Daglar");
+  console.log(tx);
+  console.log("added");
+};
+
+export const getParent = async () => {
+  console.log("getParent");
+  const parent = await contract.getParent();
+  console.log(parent.firstName);
+  console.log(parent);
+  console.log("get");
 };
 
 export const getContractAddress = () => {
