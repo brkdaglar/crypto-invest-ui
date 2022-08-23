81499106cc12fa77c3011e5d6e0e8850dbcf7889
Reyta Gül MURAN <84520566+reytamuran@users.noreply.github.com>
2022-08-23 16:08:31 +0300

    Admin Paneli ve Orders History

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,24 +1,38 @@
 import React from "react";
-import {
+/*import {
   connectWallet,
   addParent,
   getParent,
-} from "../shared/contractDeploy.js";
-import { useNavigate } from "react-router-dom";
+} from "../shared/contractDeploy.js";*/
+/*import { useNavigate } from "react-router-dom"; */
+
+import UsersSearch from "./Admin/UsersSearch";
 
 const MainPage = () => {
-  let navigate = useNavigate();
+  /*let navigate = useNavigate();
 
   async function handleSubmit(event) {
     event.preventDefault();
-    navigate("../kids", { replace: true });
-  }
+    navigate("../kids", { replace: true }); } */
+
   return (
-    <div>
-      <button onClick={connectWallet}>ConnectWallet</button>
-      <button onClick={addParent}> addParent </button>
-      <button onClick={handleSubmit}> Next</button>
-    </div>
+
+    <UsersSearch>
+
+    </UsersSearch>
+
+
+    /*<OrdersSearch>
+
+    </OrdersSearch> */
+
+
+    /* <div>
+       
+       <button onClick={connectWallet}>ConnectWallet</button>
+       <button onClick={addParent}> addParent </button>
+       <button onClick={handleSubmit}> Next</button> 
+     </div>  */
   );
 };
 
