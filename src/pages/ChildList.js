f9f43074605daf913bf5a6c37d789f1f0021c7ce
brkdglr <dagburak3@gmail.com>
2022-08-23 16:11:21 +0300

    roleBasedAndChild

--- src/pages/ChildList.js
+++ src/pages/ChildList.js
@@ -118,58 +118,9 @@ const ChildList = () => {
       </h1>
 
       <table id="children">
-        {/*<tr>
-          <th>Name</th>
-          <th>Surname</th>
-          <th>Birth Date</th>
-          <th>Public Adress</th>
-          <th> Balance </th>
-          <th> </th>
-          <th> </th>
-        </tr>*/}
         <div>
           <ChildsListItem childsArray={childsArray} />
         </div>
-        {/*
-        <tr>
-          <td>İlkkan</td>
-          <td>Kurt</td>
-          <td>18/04/2006</td>
-          <td>F61th6gd8J08s7H7d</td>
-          <td>7 ETH </td>
-          <td>
-            <button type="button">Send </button>
-          </td>
-          <td>
-            <button type="button">Withdraw </button>
-          </td>
-        </tr>
-        <tr>
-          <td>Ecemşah</td>
-          <td>Kurt</td>
-          <td>22/07/2010</td>
-          <td>Qx1m3gd8J0K7Oph2</td>
-          <td>11 ETH </td>
-          <td>
-            <button type="button">Send </button>
-          </td>
-          <td>
-            <button type="button">Withdraw </button>
-          </td>
-        </tr>
-        <tr>
-          <td>Yılmaz</td>
-          <td>Kurt</td>
-          <td>15/04/2015</td>
-          <td>Hkj381bHlaq76V51</td>
-          <td>No Data </td>
-          <td>
-            <button type="button">Send </button>
-          </td>
-          <td>
-            <button type="button">Withdraw </button>
-          </td>
-        </tr>*/}
       </table>
       <div className="">
         <Button
